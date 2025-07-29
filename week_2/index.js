34b30e2fa6cd7e29bc74f7f0e38a73b84561bdae
Rashid-Alee <rash2765@gmail.com>
2025-07-29 04:02:39 -0700

    Add week_2 with JS variables practice

---
+++ week_2/index.js
@@ -0,0 +1,14 @@
+    var my_name= "i am \"rashid ali\"learning javascript";
+
+    console.log(my_name)
+
+    var first = "this is first sentence";
+    var second = " this is second sentence";
+
+    var final = first + second;
+
+    console.log(final)
+
+
+
+
---
+++ week_2/variables.js
@@ -0,0 +1,11 @@
+const name = "Rashid Ali";
+let age = 22;
+let company = "DiveBridge";
+var instituition = "Sukkur IBA";
+
+
+console.log("name:", name);
+console.log("age:",age);
+console.log("compnay:",company);
+console.log("instituition:", instituition);
+
