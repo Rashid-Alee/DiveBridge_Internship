ea7034c958bb13cad2b375b8b73a21b955a8c3a8
Rashid-Alee <rash2765@gmail.com>
2025-07-29 07:31:25 -0700

    Built contact list with objects and forEach

---
+++ week_2/objects.js
@@ -0,0 +1,51 @@
+// let myContact = {
+//   name: "Rashid",
+//   email: "Rashid@example.com",
+//   phone: "123-456-7890",
+//   role: "Intern"
+// };
+
+// console.log( `name: ${ myContact.name}`);
+// console.log(`Email: ${myContact.email}`);
+
+
+
+
+
+
+let contacts = [
+  {
+    name: "Alice",
+    email: "alice@example.com",
+    role: "Developer"
+  },
+  {
+    name: "Bob",
+    email: "bob@example.com",
+    role: "Designer"
+  },
+  {
+    name: "Charlie",
+    email: "charlie@example.com",
+    role: "Manager"
+  }
+];
+
+console.log("contact list")
+
+contacts.forEach(function(person){
+    console.log("");
+
+    console.log(`${person.name}`);
+    console.log(`Email: ${person.email}`);
+    console.log(`Role: ${person.role}`);
+
+    console.log("");
+
+
+
+
+
+ 
+
+})
