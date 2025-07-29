ee18f0f1d945ad013f27ca62a0507de2d908cd7b
Rashid-Alee <rash2765@gmail.com>
2025-07-29 06:21:53 -0700

    Built reusable printList function with arrays and loops

---
+++ week_2/loops_arrays.js
@@ -0,0 +1,36 @@
+// console.log(shoppingList);
+// console.log("First item:", shoppingList[0]);
+// console.log("Last item:", shoppingList[3]);
+
+// console.log("🛒 Today's Shopping List:");
+
+
+function printList(items, title = "list") {
+
+    console.log(`\n${title}:`)
+
+    for (let i = 0; i < items.length; i++) {
+
+        console.log(` ${i + 1}. ${items[i]}`);
+
+
+    }
+
+}
+
+
+let shoppingList = ["milk", "bread", "eggs", "butter"];
+
+let books = ['forty rules', 'gems', 'polo caulo'];
+
+let movies = ["Inception", "Spider-Man", "Titanic"];
+
+
+
+
+
+printList(shoppingList, "Shopping List");
+
+printList(books, "Books to read");
+
+printList(movies, "Favorite Movies");
